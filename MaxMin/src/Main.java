a5a4869bc4d6f586f9eb8b52d6584c768deeb90d
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-28 16:42:14 +0530

    Maximum of integers

--- MaxMin/src/Main.java
+++ MaxMin/src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+        Integer result1 = MaximumFinder.findMaximum(3,2,1);
+        System.out.println("Test Case 1.1 Result: " + result1);
+        Integer result2 = MaximumFinder.findMaximum(2,3,1);
+        System.out.println("Test Case 1.2 Result: " + result2);
+        Integer result3 = MaximumFinder.findMaximum(1,2,3);
+        System.out.println("Test Case 1.3 Result: " + result3);
     }
 }
+
