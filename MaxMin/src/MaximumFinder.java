bcbeb7edf32836a655b0a908c661e6395008888a
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-28 17:18:54 +0530

    Max of strings

--- MaxMin/src/MaximumFinder.java
+++ MaxMin/src/MaximumFinder.java
@@ -20,6 +20,19 @@ public class MaximumFinder {
         }
         return max;
     }
+    public static String findMaximum(String str1, String str2, String str3) {
+        String max = str1;
+        if (str2.compareTo(max) > 0) {
+            max = str2;
+        }
+        if (str3.compareTo(max) > 0) {
+            max = str3;
+        }
+        return max;
+    }
 }
+
+
+
 
 
