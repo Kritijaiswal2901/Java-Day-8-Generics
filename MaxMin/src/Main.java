c5f1a7a808d16df8e89483a38dfdaf6616dbd7f2
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-28 17:12:28 +0530

    Maximum of floats

--- MaxMin/src/Main.java
+++ MaxMin/src/Main.java
@@ -6,6 +6,14 @@ public class Main {
         System.out.println("Test Case 1.2 Result: " + result2);
         Integer result3 = MaximumFinder.findMaximum(1,2,3);
         System.out.println("Test Case 1.3 Result: " + result3);
+
+        Float result4 = MaximumFinder.findMaximum(10.5f, 5.2f, 8.7f);
+        System.out.println("Test Case 2.1 Result: " + result4);
+        Float result5 = MaximumFinder.findMaximum(5.2f, 10.5f, 8.7f);
+        System.out.println("Test Case 2.2 Result: " + result5);
+        Float result6 = MaximumFinder.findMaximum(8.7f,5.2f,10.5f);
+        System.out.println("Test Case 2.3 Result: " + result6);
     }
-}
+    }
+
 
